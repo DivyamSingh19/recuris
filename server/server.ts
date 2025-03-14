d020e6d472f59915d4bd2a649daf45fa53a570bf
Divyam Singh <divyamsingh1918@gmail.com>
2025-03-14 23:24:36 +0530

    updates

---
+++ server/server.ts
@@ -0,0 +1,34 @@
+import express from "express"
+import cors from "cors"
+
+
+
+const app = express();
+const port = 4000
+
+
+app.use(express.json())
+app.use(cors())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen("Server started on :"+port)
