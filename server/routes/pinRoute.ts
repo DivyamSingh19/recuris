c4dc38da3e579248b573af4aa3140b44c42f11b7
Divyam Singh <divyamsingh1918@gmail.com>
2025-03-19 02:55:57 +0530

    updates

--- server/routes/pinRoute.ts
+++ server/routes/pinRoute.ts
@@ -1,9 +1,22 @@
-import express from "express"
+import express, { NextFunction } from "express"
 import { createPin,changePin } from "../controllers/pinController";
+import { Request,Response } from "express";
 const pinRouter =express.Router();
 
-pinRouter.post("/create",createPin);
-pinRouter.post("/change-pin",changePin)
+pinRouter.post("/create",async(req:Request,res:Response,next:NextFunction) => {
+    try {
+        createPin(req,res)
+    } catch (error) {
+        next(error)
+    }
+});
+pinRouter.post("/change-pin",async(req:Request,res:Response,next:NextFunction)=>{
+    try {
+        changePin(req,res)
+    } catch (error) {
+        next(error)
+    }
+     })
 
 
 export default pinRouter
