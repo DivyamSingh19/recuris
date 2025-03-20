98cff767a5c2f2f7015fcb7d0ababc7f0ff4de87
Divyam Singh <divyamsingh1918@gmail.com>
2025-03-20 13:18:12 +0530

    updates

---
+++ contracts/types/env.d.ts
@@ -0,0 +1,12 @@
+declare namespace NodeJS {
+    export interface ProcessEnv {
+        DIAGNOSTICCONTROL_ADDRESS: string;
+        DOCTORMANAGEMENT_ADDRESS: string;
+        LOCK_ADDRESS: string;
+        MEDACCESSCONTROL_ADDRESS: string;
+        MIGRATIONS_ADDRESS: string;
+        MULTISIGACCESS_ADDRESS: string;
+        PATIENTMANAGEMENT_ADDRESS: string;
+        PRESCRIPTIONCONTROL_ADDRESS: string;
+    }
+}
