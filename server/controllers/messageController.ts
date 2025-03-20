4900d270ab85bf2e3e903c7e970ee8c221e093a5
Divyam Singh <divyamsingh1918@gmail.com>
2025-03-20 05:49:37 +0530

    updates

--- server/controllers/messageController.ts
+++ server/controllers/messageController.ts
@@ -14,7 +14,11 @@ async function getMessages(req:Request,res:Response) {
     try {
         
     } catch (error) {
-        
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:(error as Error).message
+        })
     }
 }
 
